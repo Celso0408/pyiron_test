4872363ff7b269998249bfa0f3a89e14eebc0786
Jörg F. Unger <joergfunger@bam.de>
2022-03-10 18:19:15 +0100

    add debug info for actions test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyiron_base import Project
 
 pr = Project("some_project_name")
-job = pr.create.job.tensiletest("some_job_name")
+job = pr.create.job.tensiletest("some_job_name", delete_existing_job=True)
 # Then you can view the default input by # job.input
 # you can change the value of the inputs by
 print(job.input)
