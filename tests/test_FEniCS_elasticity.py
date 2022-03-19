ab7aa4232b37522f0e0de0798ab846c7e64be879
Jörg F. Unger <joergfunger@bam.de>
2022-03-19 07:53:20 +0100

    add test directory

---
+++ tests/test_FEniCS_elasticity.py
@@ -0,0 +1,41 @@
+
+# third party imports
+import numpy as np
+
+# local imports
+from pyiron_base import Project
+
+def test_FEniCS_elasticity():
+    pr = Project("some_project_name")
+    job = pr.create.job.FEniCSElasticity("some_job_name",
+                                    delete_existing_job=True)
+    # Then you can view the default input by # job.input
+    # you can change the value of the inputs by
+    #print(job.input)
+    job.input.height = 0.1
+    job.input.area = 0.1 #automatically computes the diameter
+    job.input.E = 1
+    job.input.nu = 0
+    job.input.displ = [0, -1, -2]
+    # job.input[key] = new_value
+    # Then you can run the job by
+    job.run()
+    # The output would be available as a dictionary via
+    #print(job["output"].to_object().to_builtin())
+    output = job["output"].to_object().to_builtin()
+
+    exact_output_stress = (job.input.E/job.input.height)*\
+        np.array(job.input.displ)
+    np.testing.assert_array_equal(output['stress'], exact_output_stress)
+
+    exact_output_force = (job.input.E*job.input.area/job.input.height)*\
+                         np.array(job.input.displ)
+    np.testing.assert_array_equal(output['force'], exact_output_force)
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+
+
